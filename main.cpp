bc2f267a66a826db9fb86d30136998a44cbe67b4
Faouzi Mohamed <faouzimohamedf@gmail.com>
2020-07-12 23:24:48 +0100

    update qrc

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QString locale = QLocale::system().name().section('_', 0, 0);
-    //locale = "fr";
+    locale = "fr";
     QTranslator translator;
-    translator.load(QString(":/lang/fzNavigator_"+locale));
+    //translator.load(QString(":/lang/fzNavigator_"+locale));
     app.installTranslator(&translator);
 
     MainWindow p(nullptr);
