f0d95594294b4b91ed0a2e0b3a6526dafd97496e
Faouzi Mohamed <faouzimohamedf@gmail.com>
2020-07-13 00:30:45 +0100

    Travis Ci build error resolved

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QString locale = QLocale::system().name().section('_', 0, 0);
-    locale = "fr";
+    //locale = "fr";
     QTranslator translator;
     translator.load(QString(":/lang/fzNavigator_"+locale));
     app.installTranslator(&translator);
