91abfaca756e5c1a8064ac61546ac6f37086f50d
Faouzi Mohamed <faouzimohamedf@gmail.com>
2020-10-03 02:08:34 +0100

    Creating static functions for for icon used in this project (humain readable)

--- src/FzGlobal.cpp
+++ src/FzGlobal.cpp
@@ -1,8 +1,96 @@
 #include "header/FzGlobal.h"
+
+
 FzGlobal::FzGlobal()
 {
-
+    
 }
+
+const QIcon& FzGlobal::windowIcon()
+{
+    static QIcon FZ_WIN_ICON(":/fznavigator_icones/web.png");
+    return FZ_WIN_ICON;
+}
+const QString &FzGlobal::windowIconName()
+{
+    const static QString path(":/fznavigator_icones/web.png");
+    return path;
+}
+
+
+const QIcon &FzGlobal::backIcon()
+{
+    static QIcon BACK_ICON(":/fznavigator_icones/left_full.svg");
+    return BACK_ICON;
+}
+const QString &FzGlobal::backIconName()
+{
+    const static QString path(":/fznavigator_icones/left_full.svg");
+    return path;
+}
+
+
+const QIcon &FzGlobal::forwardIcon()
+{
+    static QIcon FORWARD_ICON(":/fznavigator_icones/right_empty.svg");
+    return FORWARD_ICON;
+}
+const QString &FzGlobal::forwardIconName()
+{
+    const static QString path(":/fznavigator_icones/right_empty.svg");
+    return path;
+}
+
+
+const QIcon &FzGlobal::stopLoadIcon()
+{
+    static QIcon STOP_LOAD_ICON(":/fznavigator_icones/cancel.svg");
+    return STOP_LOAD_ICON;
+}
+const QString &FzGlobal::stopLoadIconName()
+{
+    const static QString path(":/fznavigator_icones/cancel.svg");
+    return path;
+}
+
+
+
+const QIcon &FzGlobal::reloadIcon()
+{
+    static QIcon RELOAD_ICON(":/fznavigator_icones/Reload.svg");
+    return RELOAD_ICON;
+}
+const QString &FzGlobal::reloadIconName()
+{
+    const static QString path(":/fznavigator_icones/Reload.svg");
+    return path;
+}
+
+
+const QIcon &FzGlobal::homeIcon()
+{
+    static QIcon HOME_ICON(":/fznavigator_icones/home_43.svg");
+    return HOME_ICON;
+}
+const QString &FzGlobal::homeIconName()
+{
+    const static QString path(":/fznavigator_icones/home_43.svg");
+    return path;
+}
+
+
+const QIcon &FzGlobal::submitIcon()
+{
+    static QIcon SUBMIT_ICON(":/fznavigator_icones/search_62.svg");
+    return SUBMIT_ICON;
+}
+const QString &FzGlobal::submitIconName()
+{
+    const static QString path(":/fznavigator_icones/search_62.svg");
+    return path;
+}
+
+
 
 /**
 * @brief return the text contained in the action, the text is translatable
--- src/header/FzGlobal.h
+++ src/header/FzGlobal.h
@@ -16,6 +16,31 @@ public:
     static const QString tr(const QAction* act);
     static bool currentActionIsNotNull(QAction *action);
     static bool isNotAnUrlSyntaxe(QString url);
+
+
+
+public : //Set Icons method
+    static const QIcon    &windowIcon();
+    static const QString& windowIconName();
+    
+    static const QIcon   &backIcon();
+    static const QString &backIconName();
+    
+    static const QIcon   &forwardIcon();
+    static const QString &forwardIconName();
+    
+    static const QIcon   &stopLoadIcon();
+    static const QString &stopLoadIconName();
+    
+    static const QIcon   &reloadIcon();
+    static const QString &reloadIconName();
+    
+    static const QIcon   &homeIcon();
+    static const QString &homeIconName();
+    
+    static const QIcon   &submitIcon();
+    static const QString &submitIconName();
+    
 };
 
 #endif // GLOBALFUNCTIONS_H
