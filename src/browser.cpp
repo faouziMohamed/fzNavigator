e57540bff24181ac75aaf06dc8b2c5927712f717
Faouzi Mohamed <faouzimohamedf@gmail.com>
2020-10-03 01:03:23 +0100

    Controling new Browser instance created (NUmber of them, remove all of them,...) [static function and members]

--- src/browser.cpp
+++ src/browser.cpp
@@ -1,12 +1,14 @@
 #include "header/browser.h"
 
+int Browser::nb_instances = 0;
+QVector<Browser*> Browser::all_instances;
 Browser::Browser(QWidget *parent, TabWidget* browserWindow) 
     : QMainWindow(parent)
-    , instance(browserWindow)
+    , tabwidget_instance(browserWindow)
 {
     //p.load(QUrl("https://faouzimohamed.github.io/"));
     if(browserWindow==nullptr){
-        newBrowserinstance(instance);
+        newInstance(tabwidget_instance);
     }
     resize(900, 530);
     setDocumentMode(true);
@@ -14,7 +16,7 @@ Browser::Browser(QWidget *parent, TabWidget* browserWindow)
     setWindowIcon(QIcon(":/fznavigator_icones/web.png"));
 }
 
-TabWidget* Browser::newBrowserinstance(TabWidget* browser){
+TabWidget* Browser::newInstance(TabWidget* browser){
   TabWidget *tabWidget;
   if(browser==nullptr){
       QWebEngineProfile *profil = QWebEngineProfile::defaultProfile();
@@ -27,9 +29,14 @@ TabWidget* Browser::newBrowserinstance(TabWidget* browser){
   setCentralWidget(tabWidget);
   connect(tabWidget, &TabWidget::windowRequired, this, [](TabWidget* browser){
      Browser *b = new Browser(nullptr);
-     b->newBrowserinstance(browser);
+     b->newInstance(browser);
      b->show();
   });
+  
+  connect(tabWidget, &TabWidget::customWindowTitleChanged,
+            [this](const QString &title){setWindowTitle(title);});
+  
+  instance_num = Browser::incrementNbInstances(this)-1;
   return tabWidget;
 }
 
@@ -38,4 +45,45 @@ void Browser::closeEvent(QCloseEvent *event){
     event->accept();
     TabWidget* b =  qobject_cast<TabWidget*>(centralWidget());
     b->close();    
+}
+
+int Browser::count(){
+    return Browser::nb_instances;
+}
+
+int Browser::incrementNbInstances(Browser* instance){
+    Browser::all_instances.append(instance);
+    return ++Browser::nb_instances;
+}
+
+int Browser::decrementNbInstances(Browser* instance){
+    Browser::all_instances.removeOne(instance);
+    return --Browser::nb_instances;
+}
+
+void Browser::removeInstance(Browser *instance)
+{
+    Browser::decrementNbInstances(instance);
+    instance->close();
+}
+
+void Browser::closeAllInstances()
+{
+    for(auto each_instance: Browser::all_instances){
+        Browser::removeInstance(each_instance);
+    }
+}
+
+void Browser::closeAllInstancesExcept(Browser *instance)
+{
+    for(auto each_instance: Browser::all_instances){
+        if(each_instance == instance){continue;}
+        Browser::removeInstance(each_instance);
+    }
+}
+
+void Browser::closeAllInstancesExcept(int instance_num)
+{
+    Browser* instance = Browser::all_instances.at(instance_num);
+    Browser::closeAllInstancesExcept(instance);
 }
--- src/header/browser.h
+++ src/header/browser.h
@@ -5,7 +5,6 @@
 #include "browsertab.h"
 #include "tabwidget.h"
 
-
 class Browser : public QMainWindow
 {
     Q_OBJECT
@@ -13,17 +12,31 @@ public:
     Browser(QWidget *parent=nullptr, TabWidget* browserWindow=nullptr);
 
 public slots:
-    TabWidget* newBrowserinstance(TabWidget *browser=nullptr);
+    TabWidget* newInstance(TabWidget *browser=nullptr);
 
 signals:
     
 protected:
     void closeEvent(QCloseEvent *event) override;
+    static int  count();
+    static int  incrementNbInstances(Browser *instance);
+    static int  decrementNbInstances(Browser *instance);
+    static void removeInstance(Browser* instance);
+    static void closeAllInstances();
+    static void closeAllInstancesExcept(Browser* instance);
+    static void closeAllInstancesExcept(int  instance_num);
+
+
+protected:
+    int instance_num;
+    static int nb_instances;
+    static QVector<Browser*> all_instances;
     
 private:
-    TabWidget* instance;
+    TabWidget* tabwidget_instance;
     
     friend class TabWidget;
 };
+
 
 #endif // BROWSER_H
